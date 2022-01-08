03a65a00d56b6d66649dbc3678f4fb23e0a13f48
unknown <jair.lopes@fatec.sp.gov.br>
2022-01-08 11:55:29 -0300

    Aplicando o conceito de nó na prática

---
+++ Estruturas de Dados em Java Introdução/com-dio-projetono/src/br/com/dio/conceito/no/encadeamento/no/Main.java
@@ -0,0 +1,33 @@
+package br.com.dio.conceito.no.encadeamento.no;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// no1 -> no2 -> no3 -> null
+		
+		No no1 = new No("Conteudo no1");
+		
+		No no2 = new No("Conteudo no2");	
+		no1.setProximoNo(no2);
+		
+		No no3 = new No("Conteudo no3");
+		no2.setProximoNo(no3);
+		
+		No no4 = new No("Conteudo no4");
+		no3.setProximoNo(no4);
+		
+		System.out.println("No1: " + no1);
+		System.out.println("No1: " + no1.getProximoNo());
+		
+		System.out.println("No1: " + no2);
+		
+		System.out.println("==============================");
+		System.out.println(no1);
+		System.out.println(no1.getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+		System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
+	}
+
+}
